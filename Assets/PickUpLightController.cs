5e6f289d58f8291a8010d2c3ce114261afb18ad0
Sofia Engvall <sofia@babythings.se>
2022-11-13 19:59:41 +0100

    New PickUps and Texture cleanup
    Added Maya PickUp Object with light and script
    Cleaned up Textures and Materials
    Also removed old git plugin

---
+++ Assets/PickUpLightController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpLightController : MonoBehaviour
+{
+    Light _light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _light.intensity = Mathf.PingPong(Time.time*1.6f, 0.4f)+0.4f;
+        //_light.intensity = (Random.Range(.5f, 1));
+        //_light.intensity = (Mathf.PingPong(Time.time * 1.6f, 0.5f) + 0.5f) + ((Random.Range(0, 0.1f)));
+        //Debug.Log(_light.intensity);
+    }
+}
